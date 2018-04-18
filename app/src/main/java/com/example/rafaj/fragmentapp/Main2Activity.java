d73824b5c20c611519dc35834e5df5166dd496b8
Rodrigo Alvarenga <tupic98@AdminProBook.localhost>
2018-04-18 00:30:50 -0500

    Added atributes to handle and parcelable

--- app/src/main/java/com/example/rafaj/fragmentapp/Main2Activity.java
+++ app/src/main/java/com/example/rafaj/fragmentapp/Main2Activity.java
@@ -3,35 +3,41 @@ package com.example.rafaj.fragmentapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
-    TextView text;
+    private TextView title, desc, mindesc;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        text = findViewById(R.id.textId);
+        title = findViewById(R.id.textId);
+        mindesc = findViewById(R.id.mindesc);
+        desc = findViewById(R.id.desc);
+        image = findViewById(R.id.imageicon);
 
         Intent callingIntent = getIntent();
         String intentAction = callingIntent.getAction();
         String intentType = callingIntent.getType();
 
         if (Intent.ACTION_SEND.equals(intentAction) && intentType != null){
-            if (intentType.equals("text/plain")){
                 handleReceivedText(callingIntent);
-            }
         }
 
     }
 
     private void handleReceivedText(Intent intent){
-        String intentText = intent.getStringExtra(Intent.EXTRA_TEXT);
+       Planet planet = intent.getExtras().getParcelable("KEY");
 
-        if (text != null){
-            text.setText(intentText);
+        if (title != null && mindesc != null && desc != null && image != null){
+            title.setText(planet.getNombre());
+            mindesc.setText(planet.getMindescription());
+            desc.setText(planet.getDescription());
+            image.setImageResource(planet.getId());
         }
     }
 }
